9beec43482a80d8afad79d533ff7f8548f24e51f
Urban Ekar <ekar.urban@gmail.com>
2024-04-16 13:55:52 +0200

    Commiting all of the files to the site!

---
+++ cesar.js
@@ -0,0 +1,10 @@
+class Clovek {
+    constructor(ime, starost, aktivnost) {
+        this.ime = ime;
+        this.starost = starost;
+        this.aktivnost = aktivnost;
+    }
+}
+
+var Tim = new Clovek("Tim", 14, "plezanje")
+var Urban = new Clovek("Urban", 14, "namizni tenis")
